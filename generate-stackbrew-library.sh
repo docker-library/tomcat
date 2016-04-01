74b0911f36d776672bc943f9efb05868d1d5d79a
Tianon Gravi <admwiggin@gmail.com>
2016-04-01 09:37:12 -0700

    Restructure repo from "X-jreY" to "X/jreY"

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-declare -A aliases
-aliases=(
+declare -A latestVariant=(
+	[6]='jre7'
+	[7]='jre7'
+	[8.0]='jre7'
+	[8.5]='jre8'
 )
-defaultVersion='8'
-defaultJava='7'
-defaultSuffix="jre${defaultJava}"
+latestVersion='8.0'
+declare -A aliases=(
+	[8.0]='8'
+)
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -17,30 +21,31 @@ url='git://github.com/docker-library/tomcat'
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
-	commit="$(cd "$version" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-	
-	majorVersion="${version%%-*}"
-	suffix="${version#*-}" # "jre7"
-	
-	fullVersion="$(grep -m1 'ENV TOMCAT_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
-	majorMinorVersion="${fullVersion%.*}"
-	
-	versionAliases=( $fullVersion-$suffix $majorMinorVersion-$suffix $majorVersion-$suffix ) # 8.0.14-jre7 8.0-jre7 8-jre7
-	if [ "$majorVersion" = "$defaultVersion" ]; then
-		versionAliases+=( $suffix ) # jre7
-	fi
-	
-	if [ "$suffix" = "$defaultSuffix" ]; then
-		versionAliases+=( $fullVersion $majorMinorVersion $majorVersion ) # 8.0.14 8.0 8
-		if [ "$majorVersion" = "$defaultVersion" ]; then
-			versionAliases+=( latest )
+	for variant in "$version"/*/; do
+		variant="$(basename "$variant")"
+		
+		commit="$(cd "$version/$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
+		fullVersion="$(grep -m1 'ENV TOMCAT_VERSION ' "$version/$variant/Dockerfile" | cut -d' ' -f3)"
+		
+		versionAliases=()
+		while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+			versionAliases+=( $fullVersion )
+			fullVersion="${fullVersion%[.-]*}"
+		done
+		versionAliases+=( $version ${aliases[$version]} )
+		
+		if [ "$variant" = "${latestVariant[$version]}" ]; then
+			versionAliases=( "${versionAliases[@]/%/-$variant}" "${versionAliases[@]}" )
+			if [ "$version" = "$latestVersion" ]; then
+				versionAliases+=( latest )
+			fi
+		else
+			versionAliases=( "${versionAliases[@]/%/-$variant}" )
 		fi
-	fi
-	
-	versionAliases+=( ${aliases[$version]} )
-	
-	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
+		
+		echo
+		for va in "${versionAliases[@]}"; do
+			echo "$va: ${url}@${commit} $version/$variant"
+		done
 	done
 done
