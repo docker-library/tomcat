004edbd80fbc5da064b48b7de61d018a88206839
Joe Ferguson <joe@infosiftr.com>
2016-06-09 10:31:02 -0700

    Convert "generate-stackbrew-library.sh" to output the new 2822-based format

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eu
 
 declare -A latestVariant=(
 	[6]='jre7'
@@ -8,46 +8,84 @@ declare -A latestVariant=(
 	[8.5]='jre8'
 	[9.0]='jre8'
 )
-latestVersion='8.0'
 declare -A aliases=(
-	[8.0]='8'
+	[8.0]='8 latest'
 	[9.0]='9'
 )
 
+self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( */ )
 versions=( "${versions[@]%/}" )
-url='git://github.com/docker-library/tomcat'
 
-echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		fileCommit \
+			Dockerfile \
+			$(git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						print $i
+					}
+				}
+			')
+	)
+}
+
+cat <<-EOH
+# this file is generated via https://github.com/docker-library/tomcat/blob/$(fileCommit "$self")/$self
+
+Maintainers: Tianon Gravi <admwiggin@gmail.com> (@tianon),
+             Joseph Ferguson <yosifkit@gmail.com> (@yosifkit)
+GitRepo: https://github.com/docker-library/tomcat.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
 
 for version in "${versions[@]}"; do
-	for variant in "$version"/*/; do
-		variant="$(basename "$variant")"
-		
-		commit="$(cd "$version/$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-		fullVersion="$(grep -m1 'ENV TOMCAT_VERSION ' "$version/$variant/Dockerfile" | cut -d' ' -f3)"
-		
+	for variant in jre7 jre8 jre9; do
+		[ -f "$version/$variant/Dockerfile" ] || continue
+
+		commit="$(dirCommit "$version/$variant")"
+
+		fullVersion="$(git show "$commit":"$version/$variant/Dockerfile" | awk '$1 == "ENV" && $2 == "TOMCAT_VERSION" { print $3; exit }')"
+
 		versionAliases=()
 		while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
 			versionAliases+=( $fullVersion )
 			fullVersion="${fullVersion%[.-]*}"
 		done
-		versionAliases+=( $version ${aliases[$version]} )
-		
+		versionAliases+=(
+			$version
+			${aliases[$version]:-}
+		)
+
+		variantAliases=( "${versionAliases[@]/%/-$variant}" )
+		variantAliases=( "${variantAliases[@]//latest-/}" )
+
 		if [ "$variant" = "${latestVariant[$version]}" ]; then
-			versionAliases=( "${versionAliases[@]/%/-$variant}" "${versionAliases[@]}" )
-			if [ "$version" = "$latestVersion" ]; then
-				versionAliases+=( latest )
-			fi
-		else
-			versionAliases=( "${versionAliases[@]/%/-$variant}" )
+			variantAliases+=( "${versionAliases[@]}" )
 		fi
-		
+
 		echo
-		for va in "${versionAliases[@]}"; do
-			echo "$va: ${url}@${commit} $version/$variant"
-		done
+		cat <<-EOE
+			Tags: $(join ', ' "${variantAliases[@]}")
+			GitCommit: $commit
+			Directory: $version/$variant
+		EOE
 	done
 done
